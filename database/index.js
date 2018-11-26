15cb6886b669fb10818f1586f419f58379bb2536
Dominic M <dominicccc@gmail.com>
2018-11-26 12:39:58 -0800

    added MONGO_URI

--- database/index.js
+++ database/index.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
-const mongoUri = 'mongodb+srv://Spencer:Spencer@cluster0-0okak.mongodb.net/trailblazer'
-//const mongoUri = 'mongodb://localhost/trailblazer';
+// const mongoUri = 'mongodb+srv://Spencer:Spencer@cluster0-0okak.mongodb.net/trailblazer'
+// const mongoUri = 'mongodb://localhost/trailblazer';
+const mongoUri = process.env.MONGO_URI;
 
 mongoose.connect(mongoUri, {useNewUrlParser:true});
 const db = mongoose.connection;
