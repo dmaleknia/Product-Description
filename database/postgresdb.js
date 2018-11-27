d6a599f99f5b255fc04038fdc3f53c32188f24ad
Dominic M <dominicccc@gmail.com>
2018-11-26 18:00:50 -0800

    working on knex config

--- database/postgresdb.js
+++ database/postgresdb.js
@@ -2,5 +2,4 @@ require('dotenv').config();
 
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require(__dirname + '/../knexfile.js')[environment];
-console.log(configuration);
 const database = require('knex')(configuration);
