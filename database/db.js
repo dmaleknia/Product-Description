28aa22adac94e0ba02b1b0458221d231b0ce8c86
Dominic M <dominicccc@gmail.com>
2018-11-26 17:40:58 -0800

    commented out needless line

--- database/db.js
+++ database/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const db = require('./index.js');
+// const db = require('./index.js');
 mongoose.Promise = global.Promise;
 
 const descriptionSchema = new mongoose.Schema({
